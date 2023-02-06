4872720f7aefa57a725cf1a16f6dd246b88ed4e6
Rosario Garcia <rosariogf041@gmail.com>
2023-02-05 21:50:25 -0500

    complete lap

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+if (true){
+    let companyName = "Scuber" 
+    console.log(companyName)
+}
+if (true){
+    const companyName = "Scuber"
+    console.log(companyName)
+}
+if (true){
+    const mostProfitableNeighborhood = "Chelsea"
+    console.log(mostProfitableNeighborhood)
+}
+if (true){
+    let mostProfitableNeighborhood = "Chelsea"
+    console.log(mostProfitableNeighborhood)
+}
+if (true){
+    const companyCeo = "Susan Smith"
+    console.log(companyCeo)
+}
+if (true){
+    let companyCeo = "Susan Smith"
+    console.log(companyCeo)
+}
